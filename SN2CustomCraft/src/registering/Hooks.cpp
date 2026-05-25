15532453a994e1ca517940796a6b733331ecb2e1
Limo <mail@vogel-julian.de>
2026-05-25 20:58:32 +0200

    Builder action data hook

--- SN2CustomCraft/src/registering/Hooks.cpp
+++ SN2CustomCraft/src/registering/Hooks.cpp
@@ -19,8 +19,10 @@ using namespace RC;
 using namespace Unreal;
 
 getRecipeT Hooks::originalGetRecipes = nullptr;
+getActionsT Hooks::originalGetActions = nullptr;
 
 std::unique_ptr<PLH::Detour> Hooks::getRecipesHook = nullptr;
+std::unique_ptr<PLH::Detour> Hooks::getActionsHook = nullptr;
 
 Unreal::TArray<UUWECraftingRecipe*> Hooks::GetRecipesHook() {
     auto recipes = originalGetRecipes();
@@ -30,6 +32,13 @@ Unreal::TArray<UUWECraftingRecipe*> Hooks::GetRecipesHook() {
         recipes.Add(const_cast<UUWECraftingRecipe*>(recipe));
     }
     return recipes;
+}
+
+Unreal::TArray<USN2BuilderActionData*> Hooks::GetActionsHook() {
+    auto actions = originalGetActions();
+    // TODO: Add actions
+    Log::Verbose("GetActionsHook called");
+    return actions;
 }
 
 uintptr_t Hooks::ScanCall(uintptr_t address, int ordinal) {
@@ -48,16 +57,22 @@ uintptr_t Hooks::ScanCall(uintptr_t address, int ordinal) {
 }
 
 void Hooks::RegisterHooks() {
-    Log::Verbose("Hooking recipe registry");
+    Log::Verbose("Scanning internal functions");
 
     const auto recipeFunc = USN2AssetRegistry::StaticClass()->GetFunction("SN2AssetRegistry", "GetAllCraftingRecipes");
-    const auto funcPtr = reinterpret_cast<uintptr_t>(*recipeFunc->ExecFunction);
-    const auto internalPtr = ScanCall(funcPtr, 1);
+    const auto recipeFuncPtr = reinterpret_cast<uintptr_t>(*recipeFunc->ExecFunction);
+    const auto recipeInternalPtr = ScanCall(recipeFuncPtr, 1);
+    Log::Verbose("Found recipe registry getter at {:p}", reinterpret_cast<void*>(recipeInternalPtr));
 
-    Log::Verbose("Found recipe registry getter at {:p}", reinterpret_cast<void*>(internalPtr));
+    const auto actionFunc = USN2AssetRegistry::StaticClass()->GetFunction("SN2AssetRegistry", "GetAllBuilderActions");
+    const auto actionFuncPtr = reinterpret_cast<uintptr_t>(*actionFunc->ExecFunction);
+    const auto actionInternalPtr = ScanCall(actionFuncPtr, 1);
+    Log::Verbose("Found builder action registry getter at {:p}", reinterpret_cast<void*>(actionInternalPtr));
+
+    Log::Verbose("Hooking internal functions");
 
     getRecipesHook = std::make_unique<PLH::x64Detour>(
-        internalPtr,
+        recipeInternalPtr,
         reinterpret_cast<uint64_t>(&GetRecipesHook),
         reinterpret_cast<uint64_t*>(&originalGetRecipes)
     );
@@ -66,6 +81,17 @@ void Hooks::RegisterHooks() {
         Log::Verbose("Successfully hooked recipe registry");
     else
         Log::Error("Failed to hook recipe registry!");
+
+    getActionsHook = std::make_unique<PLH::x64Detour>(
+        actionInternalPtr,
+        reinterpret_cast<uint64_t>(&GetActionsHook),
+        reinterpret_cast<uint64_t*>(&originalGetActions)
+    );
+
+    if (getActionsHook->hook())
+        Log::Verbose("Successfully hooked builder action registry");
+    else
+        Log::Error("Failed to hook builder action registry!");
 }
 
 void Hooks::UnregisterHooks() {
@@ -73,4 +99,9 @@ void Hooks::UnregisterHooks() {
         Log::Verbose("Successfully unhooked recipe registry");
     else
         Log::Error("Failed to unhook recipe registry!");
+
+    if (getActionsHook->unHook())
+        Log::Verbose("Successfully unhooked builder action registry");
+    else
+        Log::Error("Failed to unhook builder action registry!");
 }
--- SN2CustomCraft/src/registering/Hooks.hpp
+++ SN2CustomCraft/src/registering/Hooks.hpp
@@ -4,18 +4,23 @@
 
 #pragma once
 
-#include <SDK/UWECrafting_classes.hpp>
+#include "SDK/UWECrafting_classes.hpp"
+#include "SDK/Subnautica2_classes.hpp"
 #include "polyhook2/Detour/x64Detour.hpp"
 #include "Containers/Array.hpp"
 
 using getRecipeT = RC::Unreal::TArray<SDK::UUWECraftingRecipe*>(*)();
+using getActionsT = RC::Unreal::TArray<SDK::USN2BuilderActionData*>(*)();
 
 class Hooks {
     static getRecipeT originalGetRecipes;
+    static getActionsT originalGetActions;
 
     static std::unique_ptr<PLH::Detour> getRecipesHook;
+    static std::unique_ptr<PLH::Detour> getActionsHook;
 
     static RC::Unreal::TArray<SDK::UUWECraftingRecipe*> GetRecipesHook();
+    static RC::Unreal::TArray<SDK::USN2BuilderActionData*> GetActionsHook();
 
     static uintptr_t ScanCall(uintptr_t address, int ordinal);
 
