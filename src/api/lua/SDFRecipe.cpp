ab28a0a60704f744524f203cb81efc1994645cbb
Limo <mail@vogel-julian.de>
2026-06-28 19:19:42 +0200

    Somehow this was the fix

--- src/api/lua/SDFRecipe.cpp
+++ src/api/lua/SDFRecipe.cpp
@@ -13,15 +13,12 @@ using namespace RC;
 using namespace LuaMadeSimple;
 
 int SDFRecipe_Lua::setName(const Lua &lua) {
-    Log::Warning("Test");
-    //const auto& lua_object = lua.get_userdata<SDFRecipe_Lua>();
+    const auto& lua_object = lua.get_userdata<SDFRecipe_Lua>();
 
-    //Log::Warning("Test {:p}", (void*)lua_object.ref.get());
+    if (!lua.is_string())
+        lua.throw_error("Parameter #1 for function 'SDFRecipe:setName' must be an integer");
 
-    //if (!lua.is_string())
-    //    lua.throw_error("Parameter #1 for function 'SDFRecipe:setName' must be an integer");
-
-    //lua_object.ref->setName(std::string(lua.get_string()));
+    lua_object.ref->setName(std::string(lua.get_string()));
     return 0;
 }
 
@@ -53,7 +50,7 @@ auto SDFRecipe_Lua::construct(const Lua &lua, std::unique_ptr<SDFRecipe> &&impl)
         lua.discard_value(-1);
         lua.prepare_new_table();
         setupFunctions(table);
-        lua.new_metatable<SDFRecipe>("SDFRecipe", object.get_metamethods());
+        lua.new_metatable<SDFRecipe_Lua>("SDFRecipe", object.get_metamethods());
     }
 
     lua.transfer_stack_object(std::move(object), "SDFRecipe", object.get_metamethods());
@@ -69,6 +66,4 @@ void SDFRecipe_Lua::setupFunctions(const Lua::Table &table) {
         lua.set_string("SDFRecipe");
         return 1;
     });
-
-    table.make_global("SDFRecipe");
 }
