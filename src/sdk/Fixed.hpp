8a940c4ab00f7c6caaf4eeec7e84d57b3f58cf55
Limo <mail@vogel-julian.de>
2026-07-10 16:22:05 +0200

    Pause this tedious approach for now

--- src/sdk/Fixed.hpp
+++ src/sdk/Fixed.hpp
@@ -9,11 +9,6 @@
 using namespace RC;
 using namespace Unreal;
 
-enum class EUWEPublishedStatus;
-enum class EStoryGoalTargetType;
-struct FGameplayTag;
-struct FStoryGoalUnlock;
-
 enum class EUWEPublishedStatus : uint8
 {
     Published                                = 0,
@@ -32,6 +27,17 @@ enum class EStoryGoalTargetType : uint8
     EStoryGoalTargetType_MAX                 = 5,
 };
 
+enum class ECrafterType : uint8
+{
+    Undefined                                = 0,
+    Fabricator                               = 1,
+    BuilderTool                              = 2,
+    Refinery                                 = 3,
+    VehicleFabricator                        = 4,
+    ModificationStation                      = 5,
+    ECrafterType_MAX                         = 6,
+};
+
 struct FGameplayTag
 {
     FName TagName;
@@ -40,6 +46,22 @@ struct FGameplayTag
 struct FStoryGoalUnlock final
 {
     class UUWEStoryGoal* GoalToUnlockRef;
+    float DelayTime;
+    uint8 Pad_C[0x4];
+};
+
+struct FCraftingRecipeOutput final
+{
+    TSoftObjectPtr<class UUWEItemType> ItemType;
+    int32 NumItems;
+    uint8 Pad_2C[0x4];
+    FVector CollisionCheckSize;
+    TArray<TSoftClassPtr<UClass>> ClassesToIgnoreForCollisionCheck;
+};
+
+struct FScheduledStoryGoal final
+{
+    UUWEStoryGoal* GoalToUnlock;
     float DelayTime;
     uint8 Pad_C[0x4];
 };
--- src/sdk/Types.hpp
+++ src/sdk/Types.hpp
@@ -9,11 +9,6 @@
 using namespace RC;
 using namespace Unreal;
 
-class UTexture2D;
-class UUWEPrimaryDataAssetBase;
-class UUWEStoryGoal;
-class UUWEDatabankEntry;
-
 class UTexture2D final : public UObject {
 	ClassBase(Engine, Texture2D)
 };
@@ -23,6 +18,75 @@ class UUWEPrimaryDataAssetBase : public UObject {
 
     ClassProperty(PublishedStatus, EUWEPublishedStatus)
     ClassProperty(DeveloperNote, FString)
+};
+
+class UUWEStoryGoalRule : public UObject {
+	ClassBase(UWEStoryGoals, UWEStoryGoalRule)
+
+	ClassFunctionRet1(IsRuleSatisfied, bool, AActor*, Actor)
+};
+
+class UUWERequiredStoryGoalRule final : public UUWEStoryGoalRule
+{
+	ClassBase(UWEStoryGoals, UWERequiredStoryGoalRule)
+
+	ClassProperty(RequiredStoryGoalRef, UUWEStoryGoal*)
+};
+
+class UUWEEventTrackerWorldCountRule final : public UUWEStoryGoalRule
+{
+	ClassBase(UWEStoryGoals, UWEEventTrackerWorldCountRule)
+
+	ClassProperty(EventVerb, FGameplayTag)
+	ClassProperty(EventTag, FGameplayTag)
+	ClassProperty(EventAsset, UUWEPrimaryDataAssetBase*)
+	ClassProperty(MinimumCount, int32)
+};
+
+class UUWEEventTrackerPlayerCountRule final : public UUWEStoryGoalRule
+{
+	ClassBase(UWEStoryGoals, UWEEventTrackerPlayerCountRule)
+
+	ClassProperty(EventVerb, FGameplayTag)
+	ClassProperty(EventTag, FGameplayTag)
+	ClassProperty(EventAsset, UUWEPrimaryDataAssetBase*)
+	ClassProperty(MinimumCount, int32)
+};
+
+class UUWEStoryGoalRuleAnd final : public UUWEStoryGoalRule
+{
+	ClassBase(UWEStoryGoals, UWEStoryGoalRuleAnd)
+
+	ClassProperty(Rules, TArray<UUWEStoryGoalRule*>)
+};
+
+class UUWEStoryGoalRuleOr final : public UUWEStoryGoalRule
+{
+	ClassBase(UWEStoryGoals, UWEStoryGoalRuleOr)
+
+	ClassProperty(Rules, TArray<UUWEStoryGoalRule*>)
+};
+
+class UUWEStoryGoalRuleNegate final : public UUWEStoryGoalRule
+{
+	ClassBase(UWEStoryGoals, UWEStoryGoalRuleNegate)
+
+	ClassProperty(RuleToNegate, UUWEStoryGoalRule*)
+};
+
+class UUWEStoryGoalRuleCount final : public UUWEStoryGoalRule
+{
+	ClassBase(UWEStoryGoals, UWEStoryGoalRuleCount)
+
+	ClassProperty(Rules, TArray<UUWEStoryGoalRule*>)
+	ClassProperty(MinimumCount, int32)
+};
+
+class UUWEStoryGoalSchedule final : public UUWEPrimaryDataAssetBase
+{
+	ClassBase(UWEStoryGoals, UWEStoryGoalSchedule)
+
+	ClassProperty(ScheduledStoryGoals, TArray<FScheduledStoryGoal>)
 };
 
 class UUWEStoryGoal final : public UUWEPrimaryDataAssetBase {
@@ -72,4 +136,19 @@ class UUWECraftingRecipe final : public UUWEPrimaryDataAssetBase {
 	ClassProperty(Category, TSoftObjectPtr<UUWECraftingRecipeCategory>)
 	ClassProperty(OrderingIndex, int32)
 	ClassProperty(DuplicatesBuilderActionData, bool)
+};
+
+class UUWECraftingRecipeCategory final : public UUWEPrimaryDataAssetBase
+{
+	ClassBase(UWECrafting, UWECraftingRecipeCategory)
+
+	ClassProperty(Name_0, FText)
+	ClassProperty(Description, FText)
+	ClassProperty(CraftedBy, ECrafterType)
+	ClassProperty(Thumbnail, TSoftObjectPtr<UTexture2D>)
+	ClassProperty(OrderingIndex, int32)
+	ClassProperty(ParentCategory, TSoftObjectPtr<UUWECraftingRecipeCategory>)
+	ClassProperty(bShowWhenEmpty, bool)
+
+	ClassFunctionRet0(GetCrafterText, FText)
 };
